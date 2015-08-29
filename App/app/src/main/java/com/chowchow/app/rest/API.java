04cb69d63a494e65d01ac339b3e39d2d24d89f51
Jacky <someunusualactivity@gmail.com>
2015-08-29 16:54:39 +0800

    created classes for API preparation

---
+++ App/app/src/main/java/com/chowchow/app/rest/API.java
@@ -0,0 +1,26 @@
+package com.chowchow.app.rest;
+
+/**
+ * Created by jackychan on 29/8/15.
+ */
+public class API {
+    // Server IP address/domain name
+    private static final String SERVER_PREFIX = "";
+
+
+    /*********************
+    * API end-point URLs *
+    * ********************/
+
+    public static String getFriendsListURL() {
+        return SERVER_PREFIX + "";
+    }
+
+    public static String getLoginURL() {
+        return SERVER_PREFIX + "";
+    }
+
+    public static String getRegisterURL() {
+        return SERVER_PREFIX + "";
+    }
+}
--- App/app/src/main/java/com/chowchow/app/utils/Constants.java
+++ App/app/src/main/java/com/chowchow/app/utils/Constants.java
@@ -5,10 +5,16 @@ package com.chowchow.app.utils;
  */
 public class Constants {
 
+    /***************
+     * GCM related *
+     ***************/
     public static final String GCM_SENDER_ID = "314057307913"; // project id
     public static final String regKey = "czIcOhMgyQ0:APA91bE8klky_SSCSB_XC-2wkAP2f9q1u3w0TeioAN0GqqViNSSCMmvg9NNkHPvUtOcgWF_1usJSglurZmnsV_VtM1V1TUOAVnDREKC8xfq25fvPdB8xauR1jEtHs4fqrP5hcPpZ3kJt";
     public static final String API_KEY = "AIzaSyC4zhYlw4VYUKu4nQ779QaZFEbmEONFTwo"; // api key
-    
+
+    /********************************************
+     * MainActivity viewPager fragments related *
+     ********************************************/
     public enum MainFragment {
         SELECT_FRIENDS(0),
         HISTORY(1),
