4dd5572699ed3453fcd314e09269ef1a09714554
lk <clk01150@gmail.com>
2015-08-29 15:27:48 +0800

    test5

--- App/app/src/main/java/com/chowchow/app/MainActivity.java
+++ App/app/src/main/java/com/chowchow/app/MainActivity.java
@@ -1,14 +1,17 @@
 package com.chowchow.app;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.DialogPreference;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +21,9 @@ import com.chowchow.app.fragments.HistoryFragment;
 import com.chowchow.app.fragments.SelectFriendsFragment;
 import com.chowchow.app.fragments.SettingsFragment;
 import com.chowchow.app.utils.GcmManager;
+import com.chowchow.app.utils.InviteDialog;
+
+import org.w3c.dom.Text;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -44,8 +50,6 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         ButterKnife.bind(this);
         setupViewPager();
         //GcmManager.getInstance(this).deleteGcmTokeInBackground();
@@ -56,58 +60,19 @@ public class MainActivity extends BaseActivity {
     private void setupGCM(){
         Intent launchIntent = getIntent();
         if ("gcm_test_app_notification_click_action".equals(launchIntent.getAction())) {
-            //Log.d("123312321321312","Log.d(\"CHAT_ROOM_ID\",String.valueOf(data.getInt(UserInfoManager.CHAT_ROOM_ID)));");
             Bundle data = launchIntent.getExtras();
             data.isEmpty();
             final String room_id = data.getString("test");
             Log.v("test",room_id);
 
             if (room_id.compareTo("0")!=0) {
-                final Dialog dialog = new Dialog(this);
-                dialog.setContentView(R.layout.dialog_invite);
-                dialog.show();
-
-
-                TextView name = (TextView) dialog.findViewById(R.id.tv_name);
-                TextView invite = (TextView) dialog.findViewById(R.id.tv_invite);
-                ImageView u_icon = (ImageView) dialog.findViewById(R.id.iv_u_icon);
-                ImageView tick = (ImageView) dialog.findViewById(R.id.iv_ok);
-                ImageView cross = (ImageView) dialog.findViewById(R.id.iv_no);
-                ImageView eaten = (ImageView) dialog.findViewById(R.id.iv_eaten);
-
-                //String uid = "111198045882720";
-                String uname ="Harry";
-                name.setText(uname);
-                invite.setText(" Wanna eat with " + uname + " ?");
-                u_icon.setImageResource(R.drawable.icon_harry);
-
-                View.OnClickListener onclick =new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        clickDialog(v, dialog, room_id);
-                    }
-                };
-
-                tick.setOnClickListener(onclick);
-                cross.setOnClickListener(onclick);
-                eaten.setOnClickListener(onclick);
+                InviteDialog t = new InviteDialog(this,room_id);
+                t.show();
+                t.setName("hihi");
             }
         }
     }
 
-
-
-    private void clickDialog(View v, Dialog dialog, String room_id){
-        int itemId = v.getId();
-        if(itemId == R.id.iv_ok){
-            Log.v("join",room_id);
-        }else if(itemId == R.id.iv_no){
-            Log.v("cross",room_id);
-        }else if(itemId == R.id.iv_eaten){
-            Log.v("eaten",room_id);
-        }
-        dialog.dismiss();
-    }
 
     private void setupViewPager() {
         fragmentManager = getSupportFragmentManager();
---
+++ App/app/src/main/java/com/chowchow/app/utils/InviteDialog.java
@@ -0,0 +1,59 @@
+package com.chowchow.app.utils;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.chowchow.app.R;
+
+import java.util.zip.Inflater;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by lk on 23/8/15.
+ */
+public class InviteDialog extends Dialog implements View.OnClickListener{
+
+	//TextView name;
+	@Bind(R.id.tv_name) TextView name;
+	@Bind(R.id.tv_invite) TextView invite;
+	@Bind(R.id.iv_u_icon) ImageView u_icon;
+	@Bind(R.id.iv_ok) ImageView tick;
+	@Bind(R.id.iv_no) ImageView cross;
+	@Bind(R.id.iv_eaten) ImageView eaten;
+	String room_id;
+	public InviteDialog(Context context, String room_id){
+		super(context);
+		ButterKnife.bind(context, this);
+		setContentView(R.layout.dialog_invite);
+	}
+
+	public void setName(String inviter){
+		name.setText(inviter);
+		setInviteText(inviter);
+	}
+
+	public void setInviteText(String inviter){
+		invite.setText(" Wanna eat with " + inviter + " ?");
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v == tick){
+			Log.v("join", room_id);
+		}else if(v == cross){
+			Log.v("cross", room_id);
+		}else if(v == eaten){
+			Log.v("eaten", room_id);
+		}
+		this.dismiss();
+	}
+}
